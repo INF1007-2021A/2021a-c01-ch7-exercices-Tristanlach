eaad41783df8f7f8294f936e3fa0cdcedc970e40
Tristan Lachance <tri.lachance3@outlook.com>
2021-10-28 14:14:33 -0400

    fini

--- exercice.py
+++ exercice.py
@@ -9,6 +9,7 @@ sys.path.insert(0, "C:\\Users\\trila\\OneDrive\\Bureau\\Bacc Polytechnique\\Sess
 from exercice_6 import frequence
 import turtle
 from turtle import*
+import re
 
 # TODO: Définissez vos fonction ici
 def ellipsoide(a = 1, b = 2, c = 3, masse_volumique = 1):
@@ -18,29 +19,57 @@ def ellipsoide(a = 1, b = 2, c = 3, masse_volumique = 1):
 
     return volume, masse
 
-def draw_tree():
-    setheading(90)
-    color("green")
+#def draw_tree():
+    #setheading(90)
+    #color("green")
 
     #rappeler fonction recursive
-    draw_branch(70, 7, 35)
+    #draw_branch(70, 7, 35)
 
-    done()
+    #done()
 
-def draw_branch(branch_len, pen_size, angle):
-    if branch_len > 0:
-        pensize(pen_size)
-        forward(branch_len)
-        right(angle)
-        draw_branch(branch_len -10, pen_size -1, angle -5)
-        left(2 * angle)
-        draw_branch(branch_len -10, pen_size -1, angle -5)
-        right(angle)
-        backward(branch_len)
+#def draw_branch(branch_len, pen_size, angle):
+    #if branch_len > 0:
+        #pensize(pen_size)
+        #forward(branch_len)
+        #right(angle)
+        #draw_branch(branch_len -10, pen_size -1, angle -5)
+        #left(2 * angle)
+        #draw_branch(branch_len -10, pen_size -1, angle -5)
+        #right(angle)
+        #backward(branch_len)
+
+def valide(sequence):
+    #if sequence != "":
+        #return set(sequence).issubset("actg")
+
+    #return False
+    return bool(re.match("^[actg]+$", sequence))
+
+def saisie(type):
+    value = input(f"{type} : ")
+    if valide(value):
+        return value
+
+    print(f"La {type} n'est pas valide!")
+    return saisie(type)
+
+def proportion(chaine, sequence):
+    return chaine.count(sequence)/len(chaine)
+
+def check_dna():
+    chaine = saisie("chaine")
+    sequence = saisie("sequence")
+
+    return proportion(chaine, sequence)
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     print(ellipsoide(2, 3, 4, 2 ))
     print((lambda sentence : sorted(frequence(sentence).items(), key = lambda x: x[1])[-1][0])("bonjour, je suis une phraseee."))
-    draw_tree()
+    #draw_tree()
+    print(valide("aacccttggg"))
+    saisie("chaine")
+    print(proportion("acccttggggttttccaaaa", "actg"))
+    check_dna()
